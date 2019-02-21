6412b97b21154b00f4b3ab030b946fcb9a68d4bb
KevinRK29 <kevinrk29@hotmail.com>
2019-02-20 19:09:26 -0500

    created logout request

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -9,4 +9,14 @@ module.exports = app => {
   );
 
   app.get("/auth/google/callback", passport.authenticate("google"));
+
+  app.get("/api/logout", (req, res) => {
+    req.logout();
+    res.send(req.user);
+  });
+
+  app.get("/api/current_user", (req, res) => {
+    // req is request and res is response
+    res.send(req.user);
+  });
 };
