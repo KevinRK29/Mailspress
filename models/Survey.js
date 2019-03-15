9e3b247f3c4bbf98331a47ba743b7c9107ba8226
KevinRK29 <kevinrk29@hotmail.com>
2019-03-14 23:29:48 -0400

    created a relationship field with the user collection

--- models/Survey.js
+++ models/Survey.js
@@ -8,7 +8,10 @@ const surveySchema = new Schema({
   subject: String,
   recipients: [RecipientSchema],
   yes: { type: Number, default: 0 },
-  no: { type: Number, default: 0 }
+  no: { type: Number, default: 0 },
+  _user: { type: Schema.Types.ObjectID, ref: "User" }, // set up a relationship field with the user collection
+  dateSent: Date,
+  lastResponded: Date
 });
 
 mongoose.model("survey", surveySchema);
