e8dde4d401b8e75c5718029ef3c31d73fb69d678
KevinRK29 <kevinrk29@hotmail.com>
2019-03-14 23:09:45 -0400

    created survey model

---
+++ models/surveys.js
@@ -0,0 +1,11 @@
+const Mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const surveySchema = new Schema({
+  title: String,
+  body: String,
+  subject: String,
+  recipients: [String]
+});
+
+mongoose.model("survey", surveySchema);
