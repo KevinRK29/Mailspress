e6fe06c918ce63f8e0d9fce7e98c65bf15bb48ea
KevinRK29 <kevinrk29@hotmail.com>
2019-03-15 00:15:20 -0400

    created array of recipients and retrieved email from it

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -2,16 +2,19 @@ const mongoose = require("mongoose");
 const requireLogin = require("../middlewares/requireLogin");
 const requireCredits = require("../middlewares/requireCredits");
 
-const Survey - mongoose.model("surveys");
+const Survey = mongoose.model("surveys");
 
 module.exports = app => {
   app.post("./api/surveys", requireLogin, requireCredits, (req, res) => {
     const { title, subject, body, recipients } = req.body;
 
     const survey = new Survey({
-        title, //title: title reduced to title due to ES6
-        subject, 
-        body, 
-    })
+      title, //title: title reduced to title due to ES6
+      subject,
+      body,
+      recipients: recipients.split(",").map(email => ({ email })), //  => {return {email: email}) condensed
+      _user: req.user.id,
+      dateSent: Date.now()
+    });
   });
 };
